b8bbd2aa66475d3a1e389ae5283744911b9851d5
Daniel Benson <dbenson24@gmail.com>
2020-01-28 13:55:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get("/", function(request, response) {
 });
 
 // listen for requests :)
-const listener = app.listen(61154, function() {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
